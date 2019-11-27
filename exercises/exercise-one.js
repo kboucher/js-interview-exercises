50bf5ea0122c16521537a5933c007e15d9fb7797
Kevin Boucher <kevin@kevinboucher.com>
2019-11-27 10:01:52 -0600

    Update exercise-one.js

--- exercises/exercise-one.js
+++ exercises/exercise-one.js
@@ -50,7 +50,7 @@ function getAreaOfCircle(radius) {
 }
 
 /**
-    In mathematics, the Fibonacci numbers, commonly denoted Fn form a sequence,
+    In mathematics, the Fibonacci numbers, commonly denoted Fn, form a sequence
     called the Fibonacci sequence, such that each number is the sum of the two
     preceding ones, starting from 0 and 1.
 
